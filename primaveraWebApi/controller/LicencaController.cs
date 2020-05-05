e5a2ac0ef712a2d6c86645e8f334d0adb7528f21
guirruta <yotlanga@gmail.com>
2020-05-05 16:40:23 +0200

    #6 - Criar API do Controlador Vendedor

---
+++ primaveraWebApi/controller/LicencaController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/*
+ *
+ * Classe responsavel pelo licenciamento e sua validacao.
+ *  # verificar se a licenca ainda esta dentro da data de uso.
+ * 
+ *    
+  */
+namespace primaveraWebApi.controller
+{
+    class LicencaController
+    {
+    }
+}
---
+++ primaveraWebApi/controller/VendedorController.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using primaveraApi.crud;
+using primaveraApi.modelo;
+using System.Net.Http.Headers;
+
+namespace primaveraWebApi
+{
+    public class VendedorController : ApiController
+    {
+        // Get api/vendedor
+        public List<Vendedor> Get()
+        {
+
+            VendedorCrud vendedorCrud = new VendedorCrud();
+            List<Vendedor> listaVendedor = vendedorCrud.read();
+            return listaVendedor;
+        }
+
+        // Get api/vendedor/id
+        public IEnumerable<String> Get(String id)
+        {
+
+
+            return new string[] { "ola", "mundo" };
+        }
+
+        // POST api/artigo
+        public void Post([FromBody] String value)
+        {
+
+        }
+
+        // PUT api/artigo/id
+        public void put(int id, [FromBody]string value)
+        {
+
+        }
+
+        //DELETE api/artigo/id
+        public void Delete(int id)
+        {
+
+        }
+
+
+    }
+}
